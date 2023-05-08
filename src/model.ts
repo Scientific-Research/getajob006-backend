8552458f2dfb5acd8640fbb286a01a75ebee1d70
Maximilian <scientificresearch309@gmail.com>
2023-05-09 00:14:08 +0200

    db data Bank works but with this line of code : const db: any = new Low(adapter, { defaultValue: { posts: [], user: {} } }); This line of Code from Edward didn't work.

--- src/model.ts
+++ src/model.ts
@@ -1,10 +1,17 @@
 import fs from "fs";
 import * as model from "./model.js";
 import { Job, Skill, nullObjectSkill, TotaledSkill, Todo } from "./types.js";
-import { join, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url'
-import { Low } from 'lowdb';
-import { JSONFile } from 'lowdb/node';
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const dbFile = join(__dirname, `../src/data/db.json`);
+const adapter = new JSONFile(dbFile);
+// const db: any = new Low(adapter);
+const db: any = new Low(adapter, { defaultValue: { posts: [], user: {} } });
+await db.read();
 
 const _jobs: Job[] = JSON.parse(
   fs.readFileSync("./src/data/jobs.json", "utf8")
@@ -99,5 +106,6 @@ export const lookupSkill = (idCode: string): Skill => {
 };
 
 export const getTest = () => {
-  return "Test from Server";
+  //   return "Test from Server";
+  return db.data.test;
 };
