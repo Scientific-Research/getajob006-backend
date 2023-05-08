afd8d014cdb139e35e80be3b85e69e01ca6cd1ae
Maximilian <scientificresearch309@gmail.com>
2023-05-08 23:22:21 +0200

    app.get("/test") works!

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import * as model from './model.js';
+import express from "express";
+import cors from "cors";
+import * as model from "./model.js";
 
 const app = express();
 app.use(cors());
 const port = 3011;
 
-app.get('/', (req: express.Request, res: express.Response) => {
-	res.send(model.getApiInstructionsHtml());
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send(model.getApiInstructionsHtml());
 });
 
-app.get('/jobs', (req: express.Request, res: express.Response) => {
-	res.json(model.getJobs());
+app.get("/jobs", (req: express.Request, res: express.Response) => {
+  res.json(model.getJobs());
 });
 
-app.get('/todos', (req: express.Request, res: express.Response) => {
-	res.json(model.getTodos());
+app.get("/todos", (req: express.Request, res: express.Response) => {
+  res.json(model.getTodos());
 });
 
-app.get('/totaledSkills', (req: express.Request, res: express.Response) => {
-	res.json(model.getTotaledSkills());
+app.get("/totaledSkills", (req: express.Request, res: express.Response) => {
+  res.json(model.getTotaledSkills());
+});
+
+app.get("/test", (req: express.Request, res: express.Response) => {
+  res.send("Test from Server");
 });
 
 app.listen(port, () => {
-	console.log(`listening on http://localhost:${port}`);
+  console.log(`listening on http://localhost:${port}`);
 });
